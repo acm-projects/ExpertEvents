40c94f60cb77c039f8716dac29957d75c0460c36
Charu <charumithayuvaraja22@gmail.com>
2024-02-21 20:32:35 -0600

    user auth init

---
+++ expert_events_flutter_project/lib/firebase_auth_implementation/firebase_auth_services.dart
@@ -0,0 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthService{
+
+    FirebaseAuth _auth = FirebaseAuth.instance;
+
+    //User sign up
+    Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+        
+        try{
+            UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+            return credential.user;
+
+        } on FirebaseAuthException catch (e) {
+            
+            if (e.code == 'email-already-in-use'){
+                showToast(message: 'The email address is already in use.');
+            } else {
+                showToast(message: 'An error occured: ${e.code}');
+            }
+
+        }
+        return null;
+    }
+    
+    //User sign up
+    Future<User?> signInWithEmailAndPassword(String email, String password) async {
+        
+        try{
+            UserCredential credential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+            return credential.user;
+
+        } on FirebaseAuthException catch (e) {
+             if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+                showToast(message: 'Invalid email or password.');
+            } else {
+                showToast(message: 'An error occurred: ${e.code}');
+            }
+        }
+        return null;
+    }
+
+}
+
+//SIGN IN WITH GOOGLE 
+
+_signInWithGoogle()async{
+
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    try {
+
+      final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+
+      if(googleSignInAccount != null ){
+        final GoogleSignInAuthentication googleSignInAuthentication = await
+        googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken,
+        );
+
+        await _firebaseAuth.signInWithCredential(credential);
+        Navigator.pushNamed(context, "/home");
+      }
+
+    }catch(e) {
+showToast(message: "some error occured $e");
+    }
+
+
+  }
