96ca032c32375ac138a99b6b8c733baf7fb5d3da
SwiftCode123 <94758977+SwiftCode123@users.noreply.github.com>
2024-02-26 12:22:15 -0600

    Update mainevents.dart

--- expert_events_flutter_project/lib/mainevents.dart
+++ expert_events_flutter_project/lib/mainevents.dart
@@ -1,64 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:like_button/like_button.dart';
 
-// class Events extends StatelessWidget {
-//   const Events({super.key});
-//
-//   @override
-//   Widget build(BuildContext context)
-//   {
-//     return Scaffold (
-//       appBar: AppBar(
-//       ),
-//       body: const Center(
-//         child: Text("This is another page"),
-//       ),
-//     );
-//   }
-//
-//   //Need this for some reason :/
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     throw UnimplementedError();
-//   }
-// }
+class MainEvents extends StatelessWidget {
+  const MainEvents({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  //Name of app title
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'App Bar Demo',
+      title: 'Main events page',
       home: HomePage(),
     );
-    // TODO: implement build
   }
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
-}) : super(key: key);
+  }) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  //For bell icon turning yellow
+  bool isBellClicked = false;
+
+  //Function for building logoImage and name
+  Widget _buildLogoAndName(String logoImage, String name) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.black, // Border color
+                  width: 2.0, // Border width
+                ),
+                image: DecorationImage(
+                  image: AssetImage(logoImage),
+                  fit: BoxFit.cover, // Adjust the image fit as needed
+                ),
+              ),
+            ),
+          ),
+          Text(
+            name,
+            style: TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //Building the poster
+  Widget _buildPoster(String posterImage) {
+    return Container(
+      height: 400,
+      child: Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(posterImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //Top right corner for + sign
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey,
-        title: const Text("Main Events"),
+        title: const Text("Main"),
         centerTitle: true,
         leading: Padding(
           padding: EdgeInsets.only(left: 10.0), // Adjust the left padding as needed
           child: IconButton(
-            onPressed: (){},
+            onPressed: () {},
             icon: Image.asset('assets/experteventslogo.jpg'),
           ),
         ),
@@ -66,128 +98,85 @@ class _HomePageState extends State<HomePage>{
           Padding(
             padding: EdgeInsets.only(right: 10.0), // Adjust the right padding as needed
             child: IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(Icons.add), // Example icon, replace it with your desired icon
             ),
           ),
         ],
       ),
 
-      //Acm logo container
-      body:
-      SingleChildScrollView(
+      //Main program for the body
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: Colors.grey.shade400,
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(left: 2.0, right: 8.0, top: 10.0, bottom: 5.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2.0,
-                      ),
-                      shape: BoxShape.circle,
-                    ),// Adjust padding as needed
-                    child: CircleAvatar(
-                      backgroundColor: Colors.grey.shade100,
-                      radius: 25,
-                      backgroundImage: AssetImage("assets/acmutd_logo.jpg"),
-                      foregroundImage: AssetImage("assets/acmutd_logo.jpg"),
-                    ),
-                    width: 60, // Replace Icons.star with the desired icon
-                  ),
-                  Text(
-                    "ACM",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ],
-              ),
-            ),
+            //Call the functions to build the poster, logo, name
+            _buildLogoAndName("assets/acmutd_logo.jpg", "ACM"),
+            _buildPoster("assets/acm_poster.jpg"),
 
-            Container(
-              height: 400,
-              child: Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/acm_poster.jpg'),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            // _buildLogoAndName("assets/acmutd_logo.jpg", "ACM"),
+            // _buildPoster("assets/acm_poster.jpg"),
 
-            Column(
-              children: [
-                Row(
+            //Contains code for the bell icon turning yellow
+            Container(
+              color: Colors.white10,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
                   children: [
-                    Container(
-                      color: Colors.blue,
-                      height: 143,
-                      width: 200,
-                      child: Padding(
-                        padding: EdgeInsets.all(31),
-                        child: Text("Date: 2/2/2024\n"
-                            "Time: 7:30 PM\n"
-                            "Location: ECSS 2.415"),
-                      ),
-                    ),
-
-                    Container(
-                      color: Colors.purple,
-                      height: 143,
-                      width: 211,
-                      child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(
-                          Icons.notifications,
-                          size: 70,
-                        ), // Using built-in bell icon
-                        color: Colors.white, // Adjust icon color as needed
-                      ),
+                    Row(
+                      children: [
+                        LikeButton(
+                          animationDuration: Duration(milliseconds: 1000),
+                        ),
+                        SizedBox(width: 5),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isBellClicked = !isBellClicked;
+                            });
+                          },
+                          child: Icon(
+                            Icons.notifications,
+                            size: 30.0,
+                            color: isBellClicked ? Colors.yellow : null,
+                          ),
+                        ),
+                        SizedBox(width: 280),
+                        Icon(
+                          Icons.share,
+                          size: 30.0,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
       ),
 
-      bottomNavigationBar:
-          Container(
-            color: Colors.black,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-              child: GNav(
-                gap: 8,
-                backgroundColor: Colors.black,
-                color: Colors.white,
-                activeColor: Colors.white,
-                tabBackgroundColor: Colors.grey.shade800,
-                padding: EdgeInsets.all(16),
-                tabs: const [
-                  GButton(icon:
-                  Icons.home,
-                      text: "Home"),
-
-                  GButton(icon: Icons.favorite_border,
-                    text: "Likes",),
-
-                  GButton(icon: Icons.search,
-                    text: 'Search',),
-
-                  GButton(icon: Icons.settings,
-                    text: "Settings",),
-                ],
-              ),
-            ),
+      //Bottom navigation bar
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+          child: GNav(
+            gap: 8,
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            padding: EdgeInsets.all(16),
+            tabs: const [
+              GButton(icon: Icons.home, text: "Home"),
+              GButton(icon: Icons.favorite_border, text: "Likes",),
+              GButton(icon: Icons.search, text: 'Search',),
+              GButton(icon: Icons.settings, text: "Settings",),
+            ],
           ),
+        ),
+      ),
     );
   }
 }
-
