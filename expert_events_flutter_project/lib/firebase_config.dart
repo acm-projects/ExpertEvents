ea45453b922dae6a7652fd0a48948dc674b48c57
Reevu01 <reevu1214@gmail.com>
2024-02-25 18:17:18 -0600

    hh

---
+++ expert_events_flutter_project/lib/firebase_config.dart
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const Map<String, String> firebaseConfig = {
+  'apiKey': "AIzaSyDETWQoioLp4Db5Io5UOMHYDXuII5Lwi6Q",
+  'authDomain': "expertevents-7bbc6.firebaseapp.com",
+  'projectId': "expertevents-7bbc6",
+  'storageBucket': "expertevents-7bbc6.appspot.com",
+  'messagingSenderId': "871374619509",
+  'appId': "1:871374619509:web:0f4b1b1f0b3bbe1d41893c",
+  'measurementId': "G-VF7EVZYBFH"
+};
