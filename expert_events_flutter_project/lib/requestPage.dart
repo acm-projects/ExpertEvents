6fa75fcaaaaec5a218c01e5cd1a4c8bba4915d2f
SwiftCode123 <94758977+SwiftCode123@users.noreply.github.com>
2024-03-03 16:42:22 -0600

    Add files via upload

---
+++ expert_events_flutter_project/lib/requestPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RequestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text('Requests'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Request Page',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
