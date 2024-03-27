23831ec6f832686a0af0d25f0af5e078ce8c3a83
SwiftCode123 <94758977+SwiftCode123@users.noreply.github.com>
2024-03-27 12:23:14 -0500

    Update mainevents.dart

--- expert_events_flutter_project/lib/mainevents.dart
+++ expert_events_flutter_project/lib/mainevents.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:like_button/like_button.dart';
+import 'package:myapp/detailedevents.dart';
 import 'package:myapp/profile.dart';
-import 'package:myapp/signup.dart';
 
-import 'friendsPage.dart';
-import 'notificationsPage.dart';
+import 'package:myapp/friendsPage.dart';
+import 'package:myapp/uploadposter.dart';
+
+import 'notificationspage.dart';
 
 class MainEvents extends StatelessWidget {
   const MainEvents({Key? key}) : super(key: key);
@@ -81,10 +82,15 @@ class _HomePageState extends State<HomePage> {
             backgroundColor: Colors.black,
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.search),
+            icon: Icon(Icons.search),
             label: "Notifications",
             backgroundColor: Colors.red,
           ),
+           BottomNavigationBarItem(
+             icon: Icon(Icons.add),
+             label: "Upload",
+             backgroundColor: Colors.red,
+           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.people),
             label: "My Friends",
@@ -117,10 +123,16 @@ class _HomePageState extends State<HomePage> {
             case 2:
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => FriendsPage()),
+                MaterialPageRoute(builder: (context) => UploadPoster()),
               );
               break;
             case 3:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FriendsPage()),
+              );
+              break;
+            case 4:
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => ProfilePage()),
@@ -137,46 +149,57 @@ class _HomePageState extends State<HomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          height: 400,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(posterImage),
-              fit: BoxFit.fill,
-            ),
-          ),
+        GestureDetector(
+          //IF YOU PRESS ON THE POSTER, YOU WILL GO TO THE MORE DETAILED EVENT PAGE; NEED TO UPDATE WITH SPECIFIC EVENT INFORMATION (FROM BACK END?)
+            onTap: () {
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => EventDetailPage(eventName: 'Event B)', eventDescription: 'This is a description of the event that the user pressed. Later on, this needs to be replaced with the description that the organization loads in. I think this will be done with information stored in back end :D', eventDate: 'Today :O', image: posterImage, location: 'ECSS 2.412')),
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.symmetric(),
+              child: Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(posterImage),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            )
         ),
+
         // Contains code for the bell, heart, and share icons
         Container(
           color: Colors.grey.shade300,
           child: Padding(
-            padding: EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(10.0),
             child: Row(
               children: [
-                SizedBox(width: 10),
-                // LikeButton(
-                //   isLiked: isHeartClicked[id] ?? false,
-                //   onTap: (bool isLiked) {
-                //     setState(() {
-                //       isHeartClicked[id] = !(isHeartClicked[id] ?? false);
-                //     });
-                //     return Future.value(!isLiked);
-                //   },
-                //   animationDuration: Duration(milliseconds: 1000),
-                // ),
+                /*LikeButton(
+                  isLiked: isHeartClicked[id] ?? false,
+                  onTap: (bool isLiked) {
+                    setState(() {
+                      isHeartClicked[id] = !(isHeartClicked[id] ?? false);
+                    });
+                    return Future.value(!isLiked);
+                  },
+                  animationDuration: Duration(milliseconds: 1000),
+                ),*/
                 SizedBox(width: 5),
-                // GestureDetector(
-                //   onTap: () {
-                //     setState(() {
-                //       isBellClicked[id] = !(isBellClicked[id] ?? false);
-                //     });
-                //   },
-                //   child: Icon(
-                //     Icons.notifications,
-                //     size: 30.0,
-                //     color: isBellClicked[id] ?? false ? Colors.yellow : null,
-                //   ),
-                // ),
+                /*GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isBellClicked[id] = !(isBellClicked[id] ?? false);
+                    });
+                  },
+                  child: Icon(
+                    Icons.notifications,
+                    size: 30.0,
+                    color: isBellClicked[id] ?? false ? Colors.yellow : null,
+                  ),
+                ),*/
                 TextButton(
                   onPressed: () {
                   },
@@ -201,30 +224,28 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
 
-
-
         //for remind me button
-        // Container(
-        //   padding: EdgeInsets.all(20.0),
-        //   color: Colors.blue,
-        //   child: Row(
-        //     children: [
-        //       Text(
-        //         "Remind me",
-        //         style: TextStyle(
-        //           color: Colors.white,
-        //           fontSize: 17,
-        //           fontWeight: FontWeight.bold,
-        //         ),
-        //       ),
-        //       SizedBox(width: 5), // Add space between text and icon
-        //       Icon(
-        //         Icons.keyboard_arrow_right,
-        //         color: Colors.white, // Optionally, set icon color
-        //       ),
-        //     ],
-        //   ),
-        // ),
+        /*Container(
+          padding: EdgeInsets.all(20.0),
+          color: Colors.blue,
+          child: Row(
+            children: [
+              Text(
+                "Remind me",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 5), // Add space between text and icon
+              Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.white, // Optionally, set icon color
+              ),
+            ],
+          ),
+        ),*/
       ],
     );
   }
@@ -261,4 +282,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
